bec78e587301348865611c19f4a0d50e556c5664
Glen Mark Anduiza <glennmarkdssgithub@gmail.com>
2022-11-02 01:15:00 +0800

    Modified DSS2 Security Configuration

--- DSS2-REST-BE-V1/src/main/java/com/dss/configuration/bean/BeanConfiguration.java
+++ DSS2-REST-BE-V1/src/main/java/com/dss/configuration/bean/BeanConfiguration.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @author Glen Mark T Anduiza
+ * @version 1.0
+ * @since 10/31/2022
+ */
+
 @Configuration
 public class BeanConfiguration {
 
--- DSS2-REST-BE-V1/src/main/java/com/dss/configuration/security/SecurityConfiguration.java
+++ DSS2-REST-BE-V1/src/main/java/com/dss/configuration/security/SecurityConfiguration.java
@@ -1,11 +1,9 @@
 package com.dss.configuration.security;
 
-import com.dss.service.auth.AuthUserDetailsService;
-import com.dss.service.auth.DssUserDetailsServiceImpl;
+import com.dss.service.userdetails.DssUserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +12,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * @author Glen Mark T Anduiza
+ * @version 1.0
+ * @since 10/31/2022
+ */
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -21,67 +25,40 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private DssUserDetailsServiceImpl dssUserDetailsService;
 
-    @Autowired
-    private AuthUserDetailsService authUserDetailsService;
-
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
     }
 
-    @Configuration
-    @Order(1)
-    public class DssLoginSecurityConfiguration extends WebSecurityConfigurerAdapter{
-//        @Bean
-//        AuthenticationProvider authenticationProvider(){
-//            DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-//            provider.setUserDetailsService(authUserDetailsService);
-//            provider.setPasswordEncoder(passwordEncoder());
-//            return provider;
-//        }
-//
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.authorizeRequests()
-//                    .anyRequest()
-//                    .authenticated()
-//                    .and()
-//                    .formLogin()
-//                    .loginPage("/API/login.do")
-//                    .permitAll();
-//        }
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(dssUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 
-    @Configuration
-    @Order(2)
-    public class DssApiSecurityConfiguration extends WebSecurityConfigurerAdapter{
-        @Bean
-        AuthenticationProvider authenticationProvider(){
-            DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-            provider.setUserDetailsService(dssUserDetailsService);
-            provider.setPasswordEncoder(passwordEncoder());
-            return provider;
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-                    .antMatchers("/")
-                    .permitAll()
-                    .antMatchers("/API/registration")
-                    .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN")
-                    .antMatchers("/API/movie-catalogue")
-                    .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN")
-                    .antMatchers("/API/actor")
-                    .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN")
-                    .antMatchers("/API/reviews")
-                    .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN", "ROLE_USER")
-                    .antMatchers("/API/actor")
-                    .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN")
-                    .anyRequest()
-                    .authenticated()
-                    .and()
-                    .httpBasic();
-        }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .antMatchers("/API/registration/**")
+                .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN")
+                .antMatchers("/API/movie-catalogue/**")
+                .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN")
+                .antMatchers("/API/reviews/**")
+                .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN", "ROLE_USER")
+                .antMatchers("/API/actor/**")
+                .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/API/login.do")
+                .permitAll()
+                .and()
+                .httpBasic();
     }
 }
